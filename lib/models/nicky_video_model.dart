8abf7dd1a15aa4be2fd9ef77246cf9e05b1381ab
nicky <nic_ky08@hotmail.com>
2019-07-12 13:51:54 +0700

    Create ListVideo Success

---
+++ lib/models/nicky_video_model.dart
@@ -0,0 +1,7 @@
+class NickyVideoModel {
+  //Field
+  String name, dateil, pathImage, pathVideo;
+
+  // Construtor
+  NickyVideoModel(this.name, this.dateil, this.pathImage, this.pathVideo);
+}
--- lib/screens/show_list_video.dart
+++ lib/screens/show_list_video.dart
@@ -1,12 +1,115 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:nicky_tpa/models/nicky_video_model.dart';
+
 class ShowListVideo extends StatefulWidget {
   @override
   _ShowListVideoState createState() => _ShowListVideoState();
 }
 
 class _ShowListVideoState extends State<ShowListVideo> {
+  // Explicot
+  Firestore fireStore = Firestore.instance;
+  StreamSubscription<QuerySnapshot> subscription;
+  List<DocumentSnapshot> snapshots;
+  List<NickyVideoModel> nickyVideoModels = [];
+
+  // Method
+  @override
+  void initState() {
+    super.initState();
+    readFireStore();
+  }
+
+  Future<void> readFireStore() async {
+    CollectionReference collectionReference =
+        fireStore.collection('NickyVideo');
+    subscription = collectionReference.snapshots().listen((response) {
+      snapshots = response.documents;
+
+      for (var snapshot in snapshots) {
+        String nameVideo = snapshot.data['Name'];
+        String detailVideo = snapshot.data['Detail'];
+        String pathImage = snapshot.data['PathImage'];
+        String pathVideo = snapshot.data['PathVideo'];
+
+        NickyVideoModel nickyVideoModel =
+            NickyVideoModel(nameVideo, detailVideo, pathImage, pathVideo);
+        setState(() {
+          nickyVideoModels.add(nickyVideoModel);
+        });
+
+        //print('$pathVideo');
+      }
+    });
+  }
+
+  Widget showImage(int index) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      width: 180.0,
+      height: 120.0,
+      child: Image.network(
+        nickyVideoModels[index].pathImage,
+        fit: BoxFit.contain,
+      ),
+    );
+  }
+
+  Widget showText(int index) {
+    return Container(
+      margin: EdgeInsets.all(5.0),
+      child: Column(
+        children: <Widget>[
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(
+              nickyVideoModels[index].name,
+              style: TextStyle(
+                fontSize: 24.0,
+                color: Colors.orange[900],
+              ),
+            ),
+          ),
+          Container(
+            alignment: Alignment.topLeft,
+            child: Text(nickyVideoModels[index].dateil),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget myDivider() {
+    return Divider(
+      height: 5.0,
+      color: Colors.brown,
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Text('Show ListVideo');
+    return Container(
+      alignment: Alignment.topLeft,
+      child: ListView.builder(
+        itemCount: nickyVideoModels.length,
+        itemBuilder: (context, int index) {
+          return Container(
+            child: Column(
+              children: <Widget>[
+                Row(
+                  children: <Widget>[
+                    showImage(index),
+                    showText(index),
+                  ],
+                ),myDivider(),
+              ],
+            ),
+          );
+        },
+      ),
+    );
   }
 }
